b0c0911e70441322c8ed7ed58de749b598777a19
NH3 <orangeandy2007@gmail.com>
2025-12-18 11:05:11 +0800

    import warning

--- src/main/java/net/mcreator/administratorauthorization/network/RouterIndexPacket.java
+++ src/main/java/net/mcreator/administratorauthorization/network/RouterIndexPacket.java
@@ -8,7 +8,6 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.PacketFlow;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import org.jetbrains.annotations.NotNull;
 
